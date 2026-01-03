881b8de451f1b058132ee8db26f9b7b3ad21540b
Owen Petchey <owen.petchey@ieu.uzh.ch>
2026-01-03 08:15:09 +0100

    updates

--- _common.R
+++ _common.R
@@ -1,3 +1,10 @@
 suppressMessages(library(ggplot2))
 suppressMessages(library(tidyverse))
 suppressMessages(library(patchwork))
+suppressMessages(library(gtsummary))
+suppressMessages(library(here))
+suppressMessages(library(kableExtra))
+suppressMessages(library(ggfortify))
+suppressMessages(library(AICcmodavg)) 
+suppressMessages(library(HSAUR3)) # A couple of datasets
+suppressMessages(library(vegan))  # ordination + PERMANOVA tools

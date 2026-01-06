28ec94abbb02377b43f4a9c6caae7a4b12eea427
Owen Petchey <owen.petchey@ieu.uzh.ch>
2026-01-06 14:59:14 +0100

    update to 2026 version

---
+++ _common.R
@@ -0,0 +1,17 @@
+suppressMessages(library(ggplot2))
+suppressMessages(library(tidyverse))
+suppressMessages(library(patchwork))
+suppressMessages(library(gtsummary))
+suppressMessages(library(here))
+suppressMessages(library(kableExtra))
+suppressMessages(library(ggfortify))
+suppressMessages(library(AICcmodavg)) 
+suppressMessages(library(HSAUR3)) # A couple of datasets
+suppressMessages(library(vegan))  # ordination + PERMANOVA tools
+suppressMessages(library(car))    # Anova tools
+suppressMessages(library(broom))
+suppressMessages(library(knitr))
+suppressMessages(library(gtsummary))
+suppressMessages(library(plotly))
+suppressMessages(library(lme4))       # mixed models
+suppressMessages(library(lmerTest)) # optional: p-values for fixed effects (advanced)

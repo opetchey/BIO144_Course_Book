069100589f44fdce543dacc7efa1a0e5078c4db2
Owen Petchey <owen.petchey@ieu.uzh.ch>
2026-05-08 19:31:03 +0200

    small updates

--- _common.R
+++ _common.R
@@ -1,17 +1,27 @@
-suppressWarnings(suppressPackageStartupMessages(library(ggplot2)))
-suppressWarnings(suppressPackageStartupMessages(library(tidyverse)))
-suppressMessages(suppressPackageStartupMessages(library(patchwork)))
-suppressMessages(suppressPackageStartupMessages(library(gtsummary)))
-suppressMessages(suppressPackageStartupMessages(library(here)))
-suppressMessages(suppressPackageStartupMessages(library(kableExtra)))
-suppressMessages(suppressPackageStartupMessages(library(ggfortify)))
-suppressMessages(suppressPackageStartupMessages(library(AICcmodavg)) )
-suppressMessages(suppressPackageStartupMessages(library(HSAUR3))) # A couple of datasets
-suppressMessages(suppressPackageStartupMessages(library(vegan)))  # ordination + PERMANOVA tools
-suppressMessages(suppressPackageStartupMessages(library(car)) )   # Anova tools
-suppressMessages(suppressPackageStartupMessages(library(broom)))
-suppressMessages(suppressPackageStartupMessages(library(knitr)))
-suppressMessages(suppressPackageStartupMessages(library(gtsummary)))
-suppressMessages(suppressPackageStartupMessages(library(plotly)))
-suppressMessages(suppressPackageStartupMessages(library(lme4))  )     # mixed models
-suppressMessages(suppressPackageStartupMessages(library(lmerTest)))# optional: p-values for fixed effects (advanced)
+
+invisible(
+  capture.output(
+    suppressWarnings(
+      suppressMessages({
+        suppressPackageStartupMessages({
+          library(ggplot2)
+          library(tidyverse)
+          library(patchwork)
+          library(gtsummary)
+          library(here)
+          library(kableExtra)
+          library(ggfortify)
+          library(AICcmodavg)
+          library(HSAUR3)
+          library(vegan)
+          library(car)
+          library(broom)
+          library(knitr)
+          library(plotly)
+          library(lme4)
+          #library(lmerTest)
+        })
+      })
+    )
+  )
+)
